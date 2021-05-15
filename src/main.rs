c71b5f6906e6feed98fa2c7f0a43d5fc9a1c6f29
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-05-16 00:48:58 +0300

    _second_commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+
+//create function to have three values
+fn value_state(value1:i32, value2:i32, state:bool) -> bool {
+    //create the condition for the negative
+    if value1 < 0 && value2 <0 && state == true {
+        let state2 = true;
+        state2
+        if  { }
+    }else {
+        let state2 = false;
+        state2
+    }
+    if
+}
+fn main() {
+    //create value for value1 , value2 and state
+    println!("{}", value_state(2, -3, false));
+}
